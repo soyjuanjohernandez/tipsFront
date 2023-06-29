461b9266c7f1ae4adff68da1e76f4c990958ab15
juan jose pelaez <juan.pelaez@neoris.com>
2023-06-29 15:55:48 -0500

    delete page an cpm unused

--- .tea/entryFiles-development/index$.web.js
+++ .tea/entryFiles-development/index$.web.js
@@ -2,11 +2,9 @@
 require('@alipay/appx-compiler/lib/sjsEnvInit');
 require('./config$');
 
-require('../../claro-button/claro-button?hash=575d90c0564a9ec75a9b4077920c00bb78edc4fd');
 require('../../claroButton/claroButton?hash=575d90c0564a9ec75a9b4077920c00bb78edc4fd');
 require('../../pages/index/index?hash=5158fa18297db3fbaac119609b168d20fcdf1eea');
 require('../../pages/boxModel/boxModel?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
 require('../../pages/live/live?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
 require('../../pages/especificidad/especificidad?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
-require('../../pages/buton-paje/buton-paje?hash=9b9617fc468b9e950c8ce3fc9314307296ad1e0c');
 require('../../pages/center/center?hash=5fe565cb7af2299f51d2befc040b17770c1f7aa9');
